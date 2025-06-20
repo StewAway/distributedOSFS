13df9fc6ab375bb8b01c7715a399b188d75539c4
StewAway <manhtuphamho@gmail.com>
2025-06-20 07:40:27 +0000

    add cache_controller.h

---
+++ core_kv/cache_controller.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "lru_cache.h"
+#include <iostream>
+
+class CacheController {
+    LRUCache cache_;
+    int hits_ = 0, misses_ = 0;
+public:
+    explicit CacheController(int capacity): cache_(capacity) {}
+
+    std::optional<std::string> get(const std::string& key) {
+        auto val = cache_.get(key);
+        if (val) {
+            hits_++;
+            std::cout<<"[CacheHit] "<<key<<"\n";
+        } else {
+            misses_++;
+            std::cout<<"[CacheMiss] "<<key<<"\n";
+        }
+        return val;
+    }
+    void put(const std::string& key, const std::string& value) {
+        cache_.put(key, value);
+    }
+
+    void print_stats() const {
+        std::cout<<"[CacheStats] Hits: "<<hits_<<",Misses: "<<misses_<<"\n";
+    }
+};
